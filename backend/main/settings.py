190b5f356d9e53b47bc421778514f804e6caf195
TilakMirajkar <teeluck2003@gmail.com>
2025-03-29 16:04:44 +0530

    fixed cors_orirgins

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://aitmeduinsight.vercel.app/",
+    "https://aitmeduinsight.vercel.app",
 ]
 
 # Application definition
