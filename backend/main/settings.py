b5ee39b5916d3171ec677957f5c6510e301d800c
TilakMirajkar <teeluck2003@gmail.com>
2025-04-14 00:55:50 +0530

    CORS all

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 # ## CORS settings
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ALLOWED_ORIGINS = [
 #     "https://aitmeduinsight.vercel.app",
 # ]
