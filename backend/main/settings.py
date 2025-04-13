0ba5a6b2d50b84b15300e8359f9ed7656c6008e8
TilakMirajkar <teeluck2003@gmail.com>
2025-04-14 01:22:48 +0530

    debug true

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 ## CORS settings
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = DEBUG
 CORS_ALLOWED_ORIGINS = [
     "https://aitmeduinsight.vercel.app",
 ]
